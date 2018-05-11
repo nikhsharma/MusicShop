ffedbb9e4693995523493b054bcd46b36e9c97b0
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-05-11 14:04:46 +0100

    Create GuitarPick and DrumSticks.

---
+++ src/main/java/items/accessories/DrumSticks.java
@@ -0,0 +1,10 @@
+package items.accessories;
+
+import items.Item;
+
+public class DrumSticks extends Item {
+
+    public DrumSticks(Double buyPrice, Double sellPrice, String description) {
+        super(buyPrice, sellPrice, description);
+    }
+}
---
+++ src/main/java/items/accessories/GuitarPick.java
@@ -0,0 +1,10 @@
+package items.accessories;
+
+import items.Item;
+
+public class GuitarPick extends Item {
+
+    public GuitarPick(Double buyPrice, Double sellPrice, String description) {
+        super(buyPrice, sellPrice, description);
+    }
+}
